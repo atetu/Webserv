69cf04a5a6b50331733c5b57c9d19066bc0a84f7
Romain SANCHEZ <rosanche@e1r6p16.42.fr>
2021-01-25 15:45:49 +0100

    Test added to push and head

--- docker/tests/tests.py
+++ docker/tests/tests.py
@@ -70,25 +70,24 @@ class TestPostMethod(WebservTestCase):
 	def test_simple_file_but_no_duplicate(self):
 		self.assertPostStatusCode("/test__simple_file_2.txt", 201, "Hello World".encode("ascii"))
 		self.assertPostStatusCode("/test__simple_file_2.txt", 200, "Hello World".encode("ascii"))
-		# self.assertPostStatusCodeChunked("/test__simple_file_3.txt", 200, "HELLO")
 		self.assertGet("/test__simple_file_2.txt", "Hello World")
+	def test_simple_file_post_sh(self):
+		self.assertPostStatusCode("/_tests/x.sh", 405, "Not autorised".encode("ascii"))
 
 class TestPutMethod(WebservTestCase):
 
-    def test_simple_file_put(self):
-        self.assertPostStatusCode("/test__simple_file_put.txt", 201, "Filed created and post".encode("ascii"))
-        self.assertPutStatusCode("/test__simple_file_put.txt", 200, "Filed changed".encode("ascii"))
-        self.assertGet("/test__simple_file_put.txt", "Filed changed")
-    
-    def test_simple_file_but_double_changes_put(self):
-        self.assertPostStatusCode("/test__simple_file_put_2.txt", 201, "Filed created and post".encode("ascii"))
-        self.assertPutStatusCode("/test__simple_file_put_2.txt", 200, "Filed changed".encode("ascii"))
-        self.assertPutStatusCode("/test__simple_file_put_2.txt", 200, "Filed changed 2 times".encode("ascii"))
-        self.assertGet("/test__simple_file_put_2.txt", "Filed changed 2 times")
+	def test_simple_file_put(self):
+		self.assertPutStatusCode("/test__simple_file_put.txt", 201, "Filed changed".encode("ascii"))
+		self.assertGet("/test__simple_file_put.txt", "Filed changed")
+	
+	def test_simple_file_but_double_changes_put(self):
+		self.assertPutStatusCode("/test__simple_file_put_2.txt", 201, "Filed changed".encode("ascii"))
+		self.assertPutStatusCode("/test__simple_file_put_2.txt", 200, "Filed changed 2 times".encode("ascii"))
+		self.assertGet("/test__simple_file_put_2.txt", "Filed changed 2 times")
 
 class TestDeleteMethod(WebservTestCase):
 	
-	def test_simple_file(self):
+	def test_simple_file_delete(self):
 		self.assertPostStatusCode("/test__will_be_deleted.txt", 201, "Hello World".encode("ascii"))
 		self.assertDeleteStatusCode("/test__will_be_deleted.txt", 200)
 	
@@ -97,35 +96,35 @@ class TestDeleteMethod(WebservTestCase):
 
 class TestOptionsMethod(WebservTestCase):
 	
-	def test_simple_file(self):
+	def test_simple_file_options_base(self):
 		self.assertOptionStatusCode("/", 200)
-	def test_simple_file(self):
+	def test_simple_file_options_tests(self):
 		self.assertOptionStatusCode("/_tests", 200)
-	def test_simple_file(self):
+	def test_simple_file_options_path(self):
 		self.assertOptionStatusCode("/wrong_path", 200)
 
 class TestHeadMethod(WebservTestCase):
 	
-	def test_simple_file(self):
+	def test_simple_file_head_base(self):
 		self.assertHeadStatusCode("/", 200)
-	def test_simple_file(self):
-		self.assertHeadStatusCode("/_tests/x.sh", 200)
-	def test_simple_file(self):
+	def test_simple_file_head_path(self):
 		self.assertHeadStatusCode("/wrong_path", 404)
-	def test_simple_file(self):
+	def test_simple_file_head_tests(self):
 		self.assertHeadStatusCode("/_tests", 404)
 
 
 if __name__ == '__main__':
 
-    def remove_if_exists(path):
-        if os.path.exists(path):
-            os.remove(path)
-    
-    remove_if_exists("test__simple_file.txt")
-    remove_if_exists("test__simple_file_2.txt")
-    remove_if_exists("test__simple_file_put.txt")
-    remove_if_exists("test__will_be_deleted.txt")
-    remove_if_exists("test__cannot_be_deleted_because_not_exists.txt")
-    
-    unittest.main()
+	def remove_if_exists(path):
+		if os.path.exists(path):
+			os.remove(path)
+	
+	remove_if_exists("test__simple_file.txt")
+	remove_if_exists("test__simple_file_2.txt")
+	remove_if_exists("test__simple_file.txt")
+	remove_if_exists("test__simple_file_put.txt")
+	remove_if_exists("test__simple_file_put_2.txt")
+	remove_if_exists("test__will_be_deleted.txt")
+	remove_if_exists("test__cannot_be_deleted_because_not_exists.txt")
+	
+	unittest.main()
